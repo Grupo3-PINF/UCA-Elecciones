33d86267340bc822740f3a29d640b9439f90fea2
Adrian <adrian.crespodelgado@alum.uca.es>
2019-11-05 19:19:36 +0100

    Login
    Login

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -9,6 +9,7 @@
 	<!-- CSS -->
 
 	<link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 	<!-- Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,16 +4,17 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			<form action="LoginController" method="POST">
+			<form action="{{ url('/LoginController') }}" method="POST">
 				<div class="form-group">
 					<input placeholder="Username" type="text" name="username">
 					<input placeholder="Password" type="password" name="password">
+					<button type="submit" class="btn btn-primary">Enviar</button>
 				</div>
 			</form>
 		</div>
 	</div>
 </div>
 
-@stop
+@endsection
 
 @extends('footer')
