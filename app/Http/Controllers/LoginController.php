9ec0fcc05cd23a02a10c02a092db88fa0b69f6ea
Adrian <adrian.crespodelgado@alum.uca.es>
2019-11-05 15:37:08 +0100

    Controlador de login
    Ejemplo de controlador de Login (falta terminarlo)

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,4 +16,27 @@ class LoginController extends Controller {
 	    Auth::logout();
 	    return Redirect::to('login');
   	}
+
+  	public function login()
+  	{
+  		if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['password']) && !empty($_POST['password']))
+  		{
+  			$username = $_POST['username'];
+  			$password = $_POST['password'];
+  			$user = User::where('username',$username)->where('password',$password)->first();
+  			if($user)
+  			{
+  				Auth::login($user);
+  				Redirect::to('index');
+  			}
+  			//unset($_SESSION['usernotfound']);
+  		} else {
+  			$mensaje = "El usuario o la contraseña son erróneos";
+  			//$_SESSION['usernotfound'] = $mensaje;
+  			//O
+  			//Session::flash('message',$mensaje);
+  			//O
+  			return Redirect::to('login')->with('mensaje',$mensaje);
+  		}
+  	}
 }
