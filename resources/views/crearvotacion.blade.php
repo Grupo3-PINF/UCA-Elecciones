be05399c5d2fa4f65c0ade037b130ce8541c46c9
Adrian <adrian.crespodelgado@alum.uca.es>
2019-11-13 11:13:08 +0100

    Index modificado
    Página de home modificada para la presentación

--- resources/views/crearvotacion.blade.php
+++ resources/views/crearvotacion.blade.php
@@ -11,24 +11,24 @@
 		</div>
 		<div class="helper">
 			<div class="step-1 row">
-				<div class="col-12 col-sm-4">
+				<div class="col-12 col-md-4">
 					<div id="votacion-pregunta" class="votacion">
 						<h4>Preguntas</h4>
 						<i class="far fa-question-circle"></i>
 						<p>Crea una votación simple en la que los votantes pueden expresar su acuerdo o desacuerdo respecto a un tema.</p>
 					</div>
 				</div>
-				<div class="col-12 col-sm-4">
+				<div class="col-12 col-md-4">
 					<div id="votacion-encuesta" class="votacion">
 						<h4>Encuesta</h4>
 						<i class="far fa-envelope"></i>
 						<p>Crea una encuesta para obtener un sondeo sobre la opinión de un grupo de personas respecto a un tema.</p>
 					</div>
 				</div>
-				<div class="col-12 col-sm-4">
+				<div class="col-12 col-md-4">
 					<div id="votacion-desarrollo" class="votacion">
 						<h4>Elecciones</h4>
-						<i class="far fa-file-code"></i>
+						<i class="fas fa-graduation-cap"></i>
 						<p>Crea una votación en la que los votantes eligen a un candidato de entre los que se hayan presentado.</p>
 					</div>
 				</div>
@@ -39,12 +39,12 @@
 						<h4>¿Quieres que sea vinculante?</h4>
 						<p>El resultado de la votación será secreto hasta que esta misma haya finalizado. Si es vinculante, el resultado será secreto hasta el final, si no lo es, el resultado podrá consultarse en el mismo momento en el que alguien realiza la votación. Pensada para preguntas importantes.</p>
 					</div>
-					<div class="col-12 col-sm-4 offset-sm-1 votacion" onclick="guardarResultado(2,'si')">
+					<div class="col-12 col-md-4 offset-md-1 votacion" onclick="guardarResultado(2,'si')">
 						<h5>Sí</h5>
 						<i class="far fa-check-circle"></i>
 						<p>Recomendada para preguntas importantes.</p>
 					</div>
-					<div class="col-12 col-sm-4 offset-sm-2 votacion" onclick="guardarResultado(2,'no')">
+					<div class="col-12 col-md-4 offset-md-2 votacion" onclick="guardarResultado(2,'no')">
 						<h5>No</h5>
 						<i class="far fa-times-circle"></i>
 						<p>Recomendada para realizar un sondeo.</p>
@@ -55,12 +55,12 @@
 						<h4>¿Quieres que sea compleja?</h4>
 						<p>Con esta opción puedes dotar de distintas opciones para la pregunta. Si la pregunta es simple, las respuestas serán: sí, no, abstenerse.</p>
 					</div>
-					<div class="col-12 col-sm-4 offset-sm-1 votacion" onclick="guardarResultado(3,'si')">
+					<div class="col-12 col-md-4 offset-md-1 votacion" onclick="guardarResultado(3,'si')">
 						<h5>Sí</h5>
 						<i class="far fa-check-circle"></i>
 						<p>Recomendada para agregar más opciones.</p>
 					</div>
-					<div class="col-12 col-sm-4 offset-sm-2 votacion" onclick="guardarResultado(3,'no')">
+					<div class="col-12 col-md-4 offset-md-2 votacion" onclick="guardarResultado(3,'no')">
 						<h5>No</h5>
 						<i class="far fa-times-circle"></i>
 						<p>Recomendada para que la pregunta sea simple.</p>
@@ -71,12 +71,12 @@
 						<h4>¿Quieres que sea restringida?</h4>
 						<p>Con esta opción, la pregunta sólo estará disponible para un determinado grupo de usuarios (a su elección).</p>
 					</div>
-					<div class="col-12 col-sm-4 offset-sm-1 votacion" onclick="guardarResultado(4,'si')">
+					<div class="col-12 col-md-4 offset-md-1 votacion" onclick="guardarResultado(4,'si')">
 						<h5>Sí</h5>
 						<i class="far fa-check-circle"></i>
 						<p>Recomendada para un determinado grupo de usuarios.</p>
 					</div>
-					<div class="col-12 col-sm-4 offset-sm-2 votacion" onclick="guardarResultado(4,'no')">
+					<div class="col-12 col-md-4 offset-md-2 votacion" onclick="guardarResultado(4,'no')">
 						<h5>No</h5>
 						<i class="far fa-times-circle"></i>
 						<p>Recomendada para el acceso de cualquier usuario.</p>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,6 +24,7 @@
 	<body>
 		@include('layouts/header')
 		@yield('content')
+		
 		@include('layouts/footer')
 	</body>
 </html>
