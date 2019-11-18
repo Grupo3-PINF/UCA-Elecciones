2a9cc0fca70c97290835310ca9459be488d53165
Kaiser16 <jesusmarquezdelgado@gmail.com>
2019-11-18 23:07:32 +0100

    Amonos Bro

---
+++ database/migrations/2019_11_18_215833_participacion.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Participacion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participacion', function (Blueprint $table) {
+            $table->bigInteger('idpregunta');
+            $table->bigInteger('idusuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participacion');
+    }
+}
