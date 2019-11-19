6ae5a42f8bfeddc42f81023d9c21394f0ffbf9bf
Adrian <adrian.crespodelgado@alum.uca.es>
2019-11-19 16:38:23 +0100

    Css resultados
    CSS

--- resources/views/resultados.blade.php
+++ resources/views/resultados.blade.php
@@ -10,10 +10,8 @@
 			</div>
 		</div>
         <div class="row">
-			<div class="col-7">
-           
+			<div class="col-12 col-md-6 offset-md-3">
                 <canvas id="doughnutChart" width="30" height="20"></canvas>
-                
 			</div>
 		</div>
 	</div>
@@ -23,18 +21,18 @@
     //doughnut
     var ctxD = document.getElementById("doughnutChart").getContext('2d');
     var myLineChart = new Chart(ctxD, {
-    type: 'doughnut',
-    data: {
-    labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
-    datasets: [{
-    data: [300, 50, 100, 40, 120],
-    backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
-    hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
-    }]
-    },
-    options: {
-    responsive: true
-    }
+        type: 'doughnut',
+        data: {
+            labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+            datasets: [{
+                data: [300, 50, 100, 40, 120],
+                backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+                hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
+            }]
+        },
+        options: {
+            responsive: true
+        }
     });
 </script>
 
