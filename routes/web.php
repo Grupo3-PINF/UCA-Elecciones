8e5a5bb8f71cd575821cd162885249f5b3708f13
Santiago Mas <santiagojesus.mas@gmail.com>
2019-11-21 12:35:07 +0100

    añadir vistas estaticas accesibilidad, aviso legal, cookies
    Corregir rutas en header y footer, añadir rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,8 @@ Route::group(
 
 Route::view('/', 'index');
 
-Route::view('legal/accesibilidad', 'legal/accesibilidad');
-Route::view('legal/avisolegal', '/avisolegal');
-Route::view('legal/cookies', 'legal/cookies');
+Route::view('/accesibilidad', 'legal/accesibilidad');
+Route::view('/avisolegal', 'legal/avisolegal');
+Route::view('/cookies', 'legal/cookies');
 
 
