1559804825eb6534e52f6f27f517b4e004813179
Santiago Mas <santiagojesus.mas@gmail.com>
2019-11-23 18:10:32 +0100

    corregir errores de merge con testing

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -30,17 +30,6 @@
 				<h2><a href="{{url ('/') }}">Portal</a></h2>
 			</div>
 			@if (Auth::check())
-<<<<<<< HEAD
-			<div class="hide-desktop">
-
-			</div>
-			<div class="hide-mobile col-12 col-sm-10">
-				<!-- <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i class="fas fa-bars fa-1x"></i></span></button> -->
-
-				<!-- Collapsible content -->
-				<!-- <div class="collapse navbar-collapse" id="navbarSupportedContent1"> -->
-
-=======
 			<div class="col-3 offset-4 hide-desktop">
 				<button class="hamburger hamburger--elastic js-hamburguer navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse" id="hamburger-menu">
 			        <span class="hamburger-box">
@@ -57,7 +46,6 @@
 				</ul>
 			</div>
 			<div class="col-12 col-sm-10 hide-mobile">
->>>>>>> fde796245ea565973be2f756304bb2351c2c771b
 				<ul class="nav">
 					<li class="nav-item"><a class="nav-link {{Request::url() === url ('/crearvotacion') ? 'active' : ''}}" href="{{url ('/crearvotacion') }}">Crear votacion</a></li>
 					<li class="nav-item"><a class="nav-link {{Request::url() === url ('/votar') ? 'active' : ''}}" href="#">Votar</a></li>
@@ -72,9 +60,6 @@
 			@endif
 		</div>
 	</div>
-<<<<<<< HEAD
-</div>
-=======
 </div>
 <script>
 	var $hamburger = $(".hamburger");
@@ -82,4 +67,3 @@
     	$hamburger.toggleClass("is-active");
   	});
 </script>
->>>>>>> fde796245ea565973be2f756304bb2351c2c771b
