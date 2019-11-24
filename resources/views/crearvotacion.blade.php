7b94ff0b05ae38d8dd6de3de1bd85e717899745d
Adrian <adrian.crespodelgado@alum.uca.es>
2019-11-24 21:06:31 +0100

    Cambio completo de crearvotacion
    Pendiente de desarrollo

--- resources/views/crearvotacion.blade.php
+++ resources/views/crearvotacion.blade.php
@@ -5,109 +5,146 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12">
-				<h3>CREADOR DE PREGUNTAS Y ENCUESTAS</h3>
+				<h3>PREGUNTAS, ELECCIONES Y CONSULTAS</h3>
 				<p>VotUCA es una herramienta intuitiva desarrollada con el fin de facilitar la creación de procesos electorales, digitalizando las votaciones y eliminando la necesidad de un escrutinio manual. El futuro es ahora.</p>
 			</div>
 		</div>
-		<form action="{{url('/crearvotacion')}}" method='POST'>
-		@csrf
-
 		@if(!@isset($mensaje))
-		<div class="helper">
-			<div class="step-1 row">
-				<div class="col-12 col-md-4">
-					<div id="votacion-pregunta" class="votacion">
-						<h4>Preguntas</h4>
-						<i class="far fa-question-circle"></i>
-						<p>Crea una votación simple en la que los votantes pueden expresar su acuerdo o desacuerdo respecto a un tema.</p>
+			<form action="{{url('/crearvotacion')}}" method='POST'>
+			@csrf
+				<div class="helper">
+					<div class="step-1 row">
+						<div class="col-12 col-md-4">
+							<div id="crear-pregunta" class="votacion" onclick="mostrarProceso('pregunta');">
+								<h4>Preguntas</h4>
+								<i class="far fa-question-circle"></i>
+								<p>Texto a aclarar con el equipo de análisis o diseño para especificar qué hace este apartado.</p>
+							</div>
+						</div>
+						<div class="col-12 col-md-4">
+							<div id="crear-eleccion" class="votacion" onclick="mostrarProceso('eleccion');">
+								<h4>Elecciones</h4>
+								<i class="fas fa-graduation-cap"></i>
+								<p>Texto a aclarar con el equipo de análisis o diseño para especificar qué hace este apartado.</p>
+							</div>
+						</div>
+						<div class="col-12 col-md-4">
+							<div id="crear-consulta" class="votacion" onclick="mostrarProceso('consulta');">
+								<h4>Consultas</h4>
+								<i class="far fa-envelope"></i>
+								<p>Texto a aclarar con el equipo de análisis o diseño para especificar qué hace este apartado.</p>
+							</div>
+						</div>
+					</div>
+					<div id="steps-pregunta" class="hide">
+						<div class="row">
+							<div class="col-12">
+								<h4>Pregunta</h4>
+								<div class="form-group">
+									<label>Título</label>
+									<input class="form-control" type="text" name="titulo-pregunta" placeholder="Ponga título a su pregunta">
+								</div>
+								<div class="form-group">
+									<label>Simple o compleja</label>
+									<select class="form-control" name="tipo-pregunta">
+										<option value="1">Simple</option>
+										<option value="2">Compleja</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label>Grupos con permiso para votar</label>
+									<select class="form-control" name="grupos-pregunta">
+										<option value="alumnos">Alumnos</option>
+										<option value="profesores">Profesores</option>
+										<option value="todos">Todos</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label>Fecha de la votación</label>
+									<input class="form-control" type="date" name="fecha-pregunta">
+								</div>
+								<div class="form-group">
+									<label>Votación anticipada</label>
+									<input type="checkbox" name="anticipada-pregunta">
+								</div>
+								<div class="form-group">
+									<label>Tiempo de votación (minutos)</label>
+									<input class="form-control" type="number" value="1">
+								</div>
+								<div class="form-group">
+									<label>Votación secreta</label>
+									<input type="checkbox" name="secreta-pregunta">
+								</div>
+								<div class="form-group">
+									<button class="btn btn-primary" type="submit">Enviar</button>
+									<a href="{{url('/crearvotacion')}}">Cancelar</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div id="steps-eleccion" class="hide">
+						<div class="row">
+							<div class="col-12">
+								<h4>Elección</h4>
+								<div class="form-group">
+									<label>Título</label>
+									<input class="form-control" type="text" name="titulo-eleccion" placeholder="Ponga título a su elección">
+								</div>
+								<div class="form-group">
+									<label>Candidatos para la elección</label>
+									<input type="text" class="form-control" name="candidatos-eleccion" placeholder="Nombre y apellidos">
+								</div>
+								<div class="form-group">
+									<label>Fecha de la elección</label>
+									<input class="form-control" type="date" name="fecha-eleccion">
+								</div>
+								<div class="form-group">
+									<label>Grupos participantes</label>
+									<select class="form-control" name="grupos-eleccion">
+										<option value="alumnos">Alumnos</option>
+										<option value="profesores">Profesores</option>
+										<option value="todos">Todos</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<label>Tipo de elección</label>
+									<select class="form-control" name="tipo-eleccion">
+										<option value="1" selected="true">Delegados</option>
+										<option value="2">Grupos no ponderados</option>
+										<option value="3">Cargos unipersonales</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<button class="btn btn-primary" type="submit">Enviar</button>
+									<a href="{{url('/crearvotacion')}}">Cancelar</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div id="steps-consulta" class="hide">
+						<div class="row">
+							<div class="col-12">
+								<h4>Consulta</h4>
+								<div class="form-group">
+									<label>Tiempo real</label>
+									<input type="checkbox" name="tiempo-consulta">
+								</div>
+								<div class="form-group">
+									<label>Tipo de consulta</label>
+									<select class="form-control" name="tipo-consulta">
+										<option value="pregunta">Pregunta</option>
+										<option value="eleccion">Eleccion</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<button class="btn btn-primary" type="submit">Enviar</button>
+									<a href="{{url('/crearvotacion')}}">Cancelar</a>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="col-12 col-md-4">
-					<div id="votacion-encuesta" class="votacion">
-						<h4>Encuesta</h4>
-						<i class="far fa-envelope"></i>
-						<p>Crea una encuesta para obtener un sondeo sobre la opinión de un grupo de personas respecto a un tema.</p>
-					</div>
-				</div>
-				<div class="col-12 col-md-4">
-					<div id="votacion-desarrollo" class="votacion">
-						<h4>Elecciones</h4>
-						<i class="fas fa-graduation-cap"></i>
-						<p>Crea una votación en la que los votantes eligen a un candidato de entre los que se hayan presentado.</p>
-					</div>
-				</div>
-			</div>
-			<div id="steps-pregunta">
-				<div class="step-2 row hide">
-					<div class="col-12">
-						<h4>¿Quieres que sea vinculante?</h4>
-						<p>El resultado de la votación será secreto hasta que esta misma haya finalizado. Si es vinculante, el resultado será secreto hasta el final, si no lo es, el resultado podrá consultarse en el mismo momento en el que alguien realiza la votación. Pensada para preguntas importantes.</p>
-					</div>
-					<div class="col-12 col-md-4 offset-md-1 votacion" onclick="guardarResultado(2,'si')">
-						<h5>Sí</h5>
-						<i class="far fa-check-circle"></i>
-						<p>Recomendada para preguntas importantes.</p>
-					</div>
-					<div class="col-12 col-md-4 offset-md-2 votacion" onclick="guardarResultado(2,'no')">
-						<h5>No</h5>
-						<i class="far fa-times-circle"></i>
-						<p>Recomendada para realizar un sondeo.</p>
-					</div>
-				</div>
-				<div class="step-3 row hide">
-					<div class="col-12">
-						<h4>¿Quieres que sea compleja?</h4>
-						<p>Con esta opción puedes dotar de distintas opciones para la pregunta. Si la pregunta es simple, las respuestas serán: sí, no, abstenerse.</p>
-					</div>
-					<div class="col-12 col-md-4 offset-md-1 votacion" onclick="guardarResultado(3,'si')">
-						<h5>Sí</h5>
-						<i class="far fa-check-circle"></i>
-						<p>Recomendada para agregar más opciones.</p>
-					</div>
-					<div class="col-12 col-md-4 offset-md-2 votacion" onclick="guardarResultado(3,'no')">
-						<h5>No</h5>
-						<i class="far fa-times-circle"></i>
-						<p>Recomendada para que la pregunta sea simple.</p>
-					</div>
-				</div>
-				<div class="step-4 row hide">
-					<div class="col-12">
-						<h4>¿Quieres que sea restringida?</h4>
-						<p>Con esta opción, la pregunta sólo estará disponible para un determinado grupo de usuarios (a su elección).</p>
-					</div>
-					<div class="col-12 col-md-4 offset-md-1 votacion" onclick="guardarResultado(4,'si')">
-						<h5>Sí</h5>
-						<i class="far fa-check-circle"></i>
-						<p>Recomendada para un determinado grupo de usuarios.</p>
-					</div>
-					<div class="col-12 col-md-4 offset-md-2 votacion" onclick="guardarResultado(4,'no')">
-						<h5>No</h5>
-						<i class="far fa-times-circle"></i>
-						<p>Recomendada para el acceso de cualquier usuario.</p>
-					</div>
-				</div>
-				<div id="pregunta-basica-div" class="step-5 row hide">
-					<div class="col-12">
-						<h4>¿Cuál es la pregunta?</h4>
-					</div>
-					<div class="col-12">
-						<label>Introduzca su pregunta</label>
-					</div>
-					<div class="col-12">
-						<textarea type="text" name="pregunta-basica" placeholder="Introduzca su pregunta"></textarea>
-					</div>
-					<input type="hidden" id="eleccion-1" value="0" name="eleccion-1">
-				<input type="hidden" id="eleccion-2" value="0" name="eleccion-2">
-				<input type="hidden" id="eleccion-3" value="0" name="eleccion-3">
-				<input type="hidden" id="eleccion-4" value="0" name="eleccion-4">
-				<input type="hidden" id="eleccion-5" value="0" name="eleccion-5">
-					<div class="col-12">
-						<button class="btn btn-primary" type="submit">Enviar</a>
-					</div>
-				</div>
-			</div>
-			</form>
-		</div>
+		</form>
 		@else
 		<div class="row">
 			<div class="col-12">
@@ -119,27 +156,14 @@
 		@endif
 	</div>
 </div>
-<script>
-	//Para la pregunta, hay distintas opciones
-	$(".step-1 #votacion-pregunta").click(function(){
-  		$(".step-1").hide(1200);
-  		$("#steps-pregunta .step-2").addClass("flex").delay(1200).queue(function(){
-    		$(this).removeClass("hide").dequeue();
-		});
-		$("#eleccion-1").val("pregunta");
-	});
-
-	function guardarResultado(paso, resultado) {
-		var next = parseInt(paso)+1;
-		$("#eleccion-"+paso).val(resultado);
-		$("#steps-pregunta .step-" + paso + " .votacion").click(function(event){
-			event.preventDefault();
-	  		$("#steps-pregunta .step-" + paso).hide(600);
-	  		$("#steps-pregunta .step-" + next).addClass("flex").delay(600).queue(function(){
-    				$(this).removeClass("hide").dequeue();
-		});
-
-	});
+<script>	
+	function mostrarProceso(tipo) {
+		$("#crear-"+tipo).click(function(){
+  			$(".step-1").hide(1200);
+  			$("#steps-"+tipo).addClass("flex").delay(1200).queue(function(){
+    			$(this).removeClass("hide").dequeue();
+			});
+  		});
 	}
 	
 	function enviarVotacion() {
