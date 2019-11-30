87e654b99ac64e66a7ae603675ff8cf0e864f01b
José Miguel Aragón Jurado <josemiguelaragonjurado@MacBook-Air-de-Jose.local>
2019-11-30 14:10:57 +0100

    Fallo middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\Admin::class,
-        'gestion' => \App\Htpp\Middleware\Gestion::class,
+        'gestion' => \App\Http\Middleware\Gestion::class,
     ];
 
     /**
