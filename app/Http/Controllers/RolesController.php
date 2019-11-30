cc0c1cd878837e5ea01e43c8599c53fd7808f8df
José Miguel Aragón Jurado <josemiguelaragonjurado@MacBook-Air-de-Jose.local>
2019-11-30 14:27:23 +0100

    Añadir y quitar rol back-end listo

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -18,15 +18,16 @@ class RolesController extends Controller
         return view('roles');
     }
 
-    public function mostrarRoles()
+    public function mostrarRoles($login)
     {
         if(session_status()==PHP_SESSION_NONE)
             session_start();
+        
+        $user = User::where('login',$login)->first();
         $array = null;
-        if(isset($_SESSION['idusuario']) && !empty($_SESSION['idusuario']))
+        if(isset($user))
         {
-            $login = $_SESSION['idusuario'];
-            $user = User::where('login',$login)->first();
+            $_SESSION['userselect']=$user->identificador;
             $rol = Rol::where('idUser',$user->identificador)->first();
 
             if(isset($rol))
@@ -45,5 +46,62 @@ class RolesController extends Controller
             }
         }
         return view('roles')->with('roles',$array);
+    }
+
+    public function añadirRol($roles)
+    {
+        if(session_status()==PHP_SESSION_NONE)
+            session_start();
+        if(isset($_SESSION['userselect']) && !isempty($_SESSION['userselect']))
+        {
+            $id = $_SESSION['userselect'];
+            $rol = Rol::where('idUser',$id)->first();
+            foreach($roles as $rol)
+            {
+                if($rol == 'Administrador')
+                    $rol->esAdmin = true;
+                if($rol == 'Secretario')
+                    $rol->esSecretario = true;
+                if($rol == 'Estudiante')
+                    $rol->esEstudiante = true;
+                if($rol == 'Desarrollador Bajo')
+                    $rol->esDesarrolladorBajo = true;
+                if($rol == 'Desarrollador Alto')
+                    $rol->esDesarrolladorAlto = true;
+            }
+            $rol->save();
+            unset($_SESSION['userselect']);
+        }
+
+        return "Roles añadidos correctamente";
+    }
+
+    public function quitarRol()
+    {
+        if(session_status()==PHP_SESSION_NONE)
+            session_start();
+        if(isset($_SESSION['userselect']) && !isempty($_SESSION['userselect']))
+        {
+            $id = $_SESSION['userselect'];
+            $rol = Rol::where('idUser',$id)->first();
+            foreach($roles as $rol)
+            {
+                if($rol == 'Administrador')
+                    $rol->esAdmin = false;
+                if($rol == 'Secretario')
+                    $rol->esSecretario = false;
+                if($rol == 'Estudiante')
+                    $rol->esEstudiante = false;
+                if($rol == 'Desarrollador Bajo')
+                    $rol->esDesarrolladorBajo = false;
+                if($rol == 'Desarrollador Alto')
+                    $rol->esDesarrolladorAlto = false;
+            }
+            $rol->save();
+            unset($_SESSION['userselect']);
+        }
+
+        return "Roles eliminados correctamente";
+} 
     }
 }
