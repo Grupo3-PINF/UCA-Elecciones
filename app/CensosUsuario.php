b05338db99b11220e2d167c2cd764046f7800006
José Miguel Aragón Jurado <josemiguelaragonjurado@wlanipr3054.uca.es>
2019-12-17 09:13:03 +0100

    Arreglado fallo CensosUsuario

--- app/CensosUsuario.php
+++ app/CensosUsuario.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Censo extends Model
+class CensosUsuario extends Model
 {
     // Nombre de la tabla
     protected $table = 'censos_usuario';
