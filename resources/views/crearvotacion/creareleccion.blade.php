3dad048189223a2913085c9395e17d63439a9f0a
nazariy <bennazariy@gmail.com>
2020-01-18 21:27:14 +0100

    Se han arreglado fallos

--- resources/views/crearvotacion/creareleccion.blade.php
+++ resources/views/crearvotacion/creareleccion.blade.php
@@ -1,159 +1,159 @@
 <div id="steps-eleccion" class="hide">
 	<div class="row">
-	<div class="row" id="input-eleccion">
-		<div class="col-12">
-			<h5>Elección</h5>
-			<div class="form-group">
-				<label>Título</label>
-				<input class="form-control" type="text" name="titulo-eleccion" placeholder="Ponga título a su elección">
-			</div>
-		</div>
-		<div class="col-12 col-md-4">
-			<div class="form-group">
-				<label>Candidatos para la elección</label>
-				<select data-live-search="true" class="form-control selectpicker" name="candidatos-eleccion"></select>
-				<a class="btn btn-secondary" id="button-candidatos">Añadir</a>
-			</div>
-		</div>
-		<div class="col-12 col-md-8">
-			<div class="form-group">
-				<label>Lista de candidatos</label>
-				<div class="w-100" id="candidatos-div-eleccion"></div>
-			</div>
-		</div>
-		<div class="col-12 col-md-4">
-			<div class="form-group">
-				<label>Grupos participantes</label>
-				<select data-live-search="true" class="form-control selectpicker" name="grupos-eleccion"></select>
-				<a class="btn btn-secondary" id="button-groups-elecciones">Añadir</a>
-			</div>
-		</div>
-		<div class="col-12 col-md-8">
-			<div class="form-group">
-				<label>Lista de grupos</label>
-				<div class="w-100" id="grupos-div-eleccion"></div>
-			</div>
-		</div>
-		<div class="col-12 col-md-4">
-			<div class="form-group">
-				<label>Fecha de la elección</label>
-				<p>Fecha y hora del comienzo de la votación. </p>
-				<input class="form-control" type="datetime-local" name="fecha-eleccion">
-			</div>
-		</div>
-		<div class="col-12 col-md-4">
-			<div class="form-group">
-				<label>Duración de votación (minutos)</label>
-				<p>Tiempo máximo para realizar la votación una vez abierta</p>
-				<input class="form-control" type="number" value="1" name="tiempo-eleccion">
-			</div>
-		</div>
-		<div class="col-12 col-md-4">
-			<div class="form-group">
-				<label>Tipo de elección</label>
-				<select class="form-control" name="tipo-eleccion">
-					<option value="1" selected>Delegados</option>
-					<option value="2">Grupos no ponderados</option>
-					<option value="3">Cargos unipersonales</option>
-				</select>
-			</div>
-		</div>
-		<div class="col-12 col-md-6" id="grupos-no-ponderados">
-			<div class="form-group">
-				<label>Grupos no ponderados</label>
-				<p>
-					¿Pueden los usuarios pertenecientes a mas de un grupo emitir un voto por cada uno de sus grupos?
-					<label><input class="test2" type="checkbox" name="multiGrupo">Si</label>
-					<label><input class="test2" type="checkbox" name="noMultiGrupo">No</label>
-				</p>
-				<p class="d-none" id="ads">¿Los votantes pertenecientes a mas de un grupo podran adscribirse al grupo que quieran votar?
-					<input type="checkbox" name="adscripcion">
-				</p>
-				<p>¿Se va votar a un porcentaje de candidatos o a un numero determinado? (Selecciona una opcion.)
-					Porcentaje: <input class="test1" type="checkbox" name="pon-por">
-					Nº candidatos: <input class="test1" type="checkbox" name="pon-num">
-				</p>
-				<div class="form-group d-none" id="porcentaje">
-					<p>Introduzca el porcentaje (Si se daja en blanco, se asigna 70% por defecto):
-						<input class="form-control" type="number" name="porCan">
-					</p>
-				</div>
-				<div class="form-group d-none" id="num-det">
-					<p>Introduce el numero de candidatos:
-						<input class="form-control" type="number" name="numCan">
-					</p>
+		<div class="row" id="input-eleccion">
+			<div class="col-12">
+				<h5>Elección</h5>
+				<div class="form-group">
+					<label>Título</label>
+					<input class="form-control" type="text" name="titulo-eleccion" placeholder="Ponga título a su elección">
 				</div>
 			</div>
-		</div>
-		<div class="col-12" id="cargos-unipersonales">
-			<div class="form-group">
-				<label>Cargos unipersonales</label>
-				<p>
-					Si deja en blanco la ponderacion de algun grupo, se le asignará una ponderacion por defecto.
-				</p>
-				<table id="example" class="display" style="width:100%">
-					<thead>
-						<tr>
-							<th>Grupo</th>
-							<th>Ponderacion(%)</th>
-						</tr>
-					</thead>
-					<tbody id="ponGrup"></tbody>
-				</table>
-				<a onclick="ponderarGrupos()" class="btn btn-secondary d-none" id="button-ponderar">Ponderar</a>
+			<div class="col-12 col-md-4">
+				<div class="form-group">
+					<label>Candidatos para la elección</label>
+					<select data-live-search="true" class="form-control selectpicker" name="candidatos-eleccion"></select>
+					<a class="btn btn-secondary" id="button-candidatos">Añadir</a>
+				</div>
 			</div>
-		</div>
+			<div class="col-12 col-md-8">
+				<div class="form-group">
+					<label>Lista de candidatos</label>
+					<div class="w-100" id="candidatos-div-eleccion"></div>
+				</div>
+			</div>
+			<div class="col-12 col-md-4">
+				<div class="form-group">
+					<label>Grupos participantes</label>
+					<select data-live-search="true" class="form-control selectpicker" name="grupos-eleccion"></select>
+					<a class="btn btn-secondary" id="button-groups-elecciones">Añadir</a>
+				</div>
+			</div>
+			<div class="col-12 col-md-8">
+				<div class="form-group">
+					<label>Lista de grupos</label>
+					<div class="w-100" id="grupos-div-eleccion"></div>
+				</div>
+			</div>
+			<div class="col-12 col-md-4">
+				<div class="form-group">
+					<label>Fecha de la elección</label>
+					<p>Fecha y hora del comienzo de la votación. </p>
+					<input class="form-control" type="datetime-local" name="fecha-eleccion">
+				</div>
+			</div>
+			<div class="col-12 col-md-4">
+				<div class="form-group">
+					<label>Duración de votación (minutos)</label>
+					<p>Tiempo máximo para realizar la votación una vez abierta</p>
+					<input class="form-control" type="number" value="1" name="tiempo-eleccion">
+				</div>
+			</div>
+			<div class="col-12 col-md-4">
+				<div class="form-group">
+					<label>Tipo de elección</label>
+					<select class="form-control" name="tipo-eleccion">
+						<option value="1" selected>Delegados</option>
+						<option value="2">Grupos no ponderados</option>
+						<option value="3">Cargos unipersonales</option>
+					</select>
+				</div>
+			</div>
+			<div class="col-12 col-md-6" id="grupos-no-ponderados">
+				<div class="form-group">
+					<label>Grupos no ponderados</label>
+					<p>
+						¿Pueden los usuarios pertenecientes a mas de un grupo emitir un voto por cada uno de sus grupos?
+						<label><input class="test2" type="checkbox" name="multiGrupo">Si</label>
+						<label><input class="test2" type="checkbox" name="noMultiGrupo">No</label>
+					</p>
+					<p class="d-none" id="ads">¿Los votantes pertenecientes a mas de un grupo podran adscribirse al grupo que quieran votar?
+						<input type="checkbox" name="adscripcion">
+					</p>
+					<p>¿Se va votar a un porcentaje de candidatos o a un numero determinado? (Selecciona una opcion.)
+						Porcentaje: <input class="test1" type="checkbox" name="pon-por">
+						Nº candidatos: <input class="test1" type="checkbox" name="pon-num">
+					</p>
+					<div class="form-group d-none" id="porcentaje">
+						<p>Introduzca el porcentaje (Si se daja en blanco, se asigna 70% por defecto):
+							<input class="form-control" type="number" name="porCan">
+						</p>
+					</div>
+					<div class="form-group d-none" id="num-det">
+						<p>Introduce el numero de candidatos:
+							<input class="form-control" type="number" name="numCan">
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-12" id="cargos-unipersonales">
+				<div class="form-group">
+					<label>Cargos unipersonales</label>
+					<p>
+						Si deja en blanco la ponderacion de algun grupo, se le asignará una ponderacion por defecto.
+					</p>
+					<table id="example" class="display" style="width:100%">
+						<thead>
+							<tr>
+								<th>Grupo</th>
+								<th>Ponderacion(%)</th>
+							</tr>
+						</thead>
+						<tbody id="ponGrup"></tbody>
+					</table>
+					<a onclick="ponderarGrupos()" class="btn btn-secondary d-none" id="button-ponderar">Ponderar</a>
+				</div>
+			</div>
 
-		<hr style="border: 0;
+			<hr style="border: 0;
 		clear:both;
 		display:block;
 		width: 96%;               
 		background-color: darkgray;
 		height: 1px;">
-		
-		<div class="col-12 col-md-4 px-4">
-			<div class="form-group">
-				<label>Votación doble</label>
-				<p>¿Se va a poder ejercer doble voto?</p>
-				<input type="checkbox" name="doblevoto">
+
+			<div class="col-12 col-md-4 px-4">
+				<div class="form-group">
+					<label>Votación doble</label>
+					<p>¿Se va a poder ejercer doble voto?</p>
+					<input type="checkbox" name="doblevoto">
+				</div>
+			</div>
+			<div class="col-12 col-md-4 px-4">
+				<div class="form-group">
+					<label>Votación secreta</label>
+					<p>Las votaciones realizadas serán públicas o no. En una votación secreta el voto es irreversible.
+					</p>
+					<input type="checkbox" name="secreta-eleccion" id="secreta-eleccion">
+				</div>
+			</div>
+			<div class="col-12 col-md-4 px-4">
+				<div class="form-group">
+					<label>Tiempo real</label>
+					<p>Por defecto, los resultados de una votación solo se pueden ver al terminarse. Esta opción permite que se puedan ver en cualquier momento.</p>
+					<input type="checkbox" name="tiempo-real-eleccion" id="tiempo-real-eleccion">
+				</div>
+			</div>
+			<div class="col-12 col-md-4 px-4">
+				<div class="form-group">
+					<label>Votación anticipada</label>
+					<p>Permite seleccionar una fecha de votación anticipada mediante un calendario.</p>
+					<input type="checkbox" name="anticipada-eleccion">
+				</div>
+			</div>
+			<div class="col-12 col-md-6 px-4" id="fecha-anticipada-eleccion">
+				<div class="form-group">
+					<label>Fecha votación anticipada</label>
+					<input class="form-control" type="datetime-local" name="fecha-anticipada-eleccion">
+				</div>
+			</div>
+			<div class="col-12 col-md-6 px-4" id="participantes-anticipada-eleccion">
+				<div class="form-group">
+					<label>Participantes de votación anticipada</label>
+					<p>Correos de las cuentas de los votantes que podrán votar de manera anticipada. Separados por ';'</p>
+					<input class="form-control" type="text" name="participantes-anticipada-eleccion">
+				</div>
 			</div>
 		</div>
-		<div class="col-12 col-md-4 px-4">
-			<div class="form-group">
-				<label>Votación secreta</label>
-				<p>Las votaciones realizadas serán públicas o no. En una votación secreta el voto es irreversible.
-				</p>
-				<input type="checkbox" name="secreta-eleccion" id="secreta-eleccion">
-			</div>
-		</div>
-		<div class="col-12 col-md-4 px-4">
-			<div class="form-group">
-				<label>Tiempo real</label>
-				<p>Por defecto, los resultados de una votación solo se pueden ver al terminarse. Esta opción permite que se puedan ver en cualquier momento.</p>
-				<input type="checkbox" name="tiempo-real-eleccion" id="tiempo-real-eleccion">
-			</div>
-		</div>
-		<div class="col-12 col-md-4 px-4">
-			<div class="form-group">
-				<label>Votación anticipada</label>
-				<p>Permite seleccionar una fecha de votación anticipada mediante un calendario.</p>
-				<input type="checkbox" name="anticipada-eleccion">
-			</div>
-		</div>
-		<div class="col-12 col-md-6 px-4" id="fecha-anticipada-eleccion">
-			<div class="form-group">
-				<label>Fecha votación anticipada</label>
-				<input class="form-control" type="datetime-local" name="fecha-anticipada-eleccion">
-			</div>
-		</div>
-		<div class="col-12 col-md-6 px-4" id="participantes-anticipada-eleccion">
-			<div class="form-group">
-				<label>Participantes de votación anticipada</label>
-				<p>Correos de las cuentas de los votantes que podrán votar de manera anticipada. Separados por ';'</p>
-				<input class="form-control" type="text" name="participantes-anticipada-eleccion">
-			</div>
-		</div>
-	</div>
 		<div class="col-12">
 			<div class="alert alert-success d-none" id="msg_div">
 				<span id="res_message"></span>
@@ -166,32 +166,22 @@
 	</div>
 </div>
 <script>
-	function navDetect() { 
-		if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
-		{
+	function navDetect() {
+		if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
 			return 'Opera';
-		}
-		else if(navigator.userAgent.indexOf("Chrome") != -1 )
-		{
+		} else if (navigator.userAgent.indexOf("Chrome") != -1) {
 			return 'Chrome';
-		}
-		else if(navigator.userAgent.indexOf("Safari") != -1)
-		{
+		} else if (navigator.userAgent.indexOf("Safari") != -1) {
 			return 'Safari'
-		}
-		else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
-		{
+		} else if (navigator.userAgent.indexOf("Firefox") != -1) {
 			return 'Firefox';
-		}
-		else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+		} else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.documentMode == true)) //IF IE > 10
 		{
-			return 'IE' 
-		}  
-		else 
-		{
+			return 'IE'
+		} else {
 			return 'unknown'
 		}
-    }
+	}
 	$('#fecha-anticipada-eleccion').hide();
 	$('#participantes-anticipada-eleccion').hide();
 	$('input[name="anticipada-eleccion"]').click(function() {
@@ -226,7 +216,7 @@
 				'porCan': $('input[name=porCan').val(),
 				'numCan': $('input[name=numCan').val(),
 				'doblevoto': $('input[name=doblevoto]').is(':checked'),
-				'navegador' : nav,
+				'navegador': nav,
 				'esTiempoReal': $('input[name=tiempo-real-eleccion]').is(':checked'),
 				'esSecreta': $('input[name=secreta-eleccion]').is(':checked'),
 				'esAnticipada': $('input[name=anticipada-eleccion]').is(':checked'),
@@ -282,6 +272,20 @@
 		})
 	}
 
+	$('#button-groups-elecciones').click(function() {
+			let nombre = $('select[name=grupos-eleccion] option:selected').text();
+			let id = $('select[name=grupos-eleccion]').val();
+			let pon = 0;
+			let html = '<input class="input-div-caja-eleccion" type="text" name="grupo-' + id + '-ponderacion-' + pon + '" placeholder="' + nombre + '" readonly><a name="borrar-' + id + '" onclick="borrarInputEleccion(\'' + id + '\',\'grupo\',\'' + pon + '\')"><i class="fas fa-window-close"></i></a>';
+			let html2 = '<tr id="tr-' + id + '" ><td>' + nombre + '</td><td><input type="text" class="input-ponderacion" name="pon-' + pon + '"></td></tr>';
+			$('#button-ponderar').removeClass('d-none');
+			console.log("Hola");
+			if (!$("#grupos-div-eleccion input[name=grupo-" + id + "-ponderacion-" + pon + "]").length) {
+				$('#grupos-div-eleccion').append(html);
+				$('#ponGrup').append(html2);
+			}
+	});
+
 	function guardarGruposElecciones() {
 		let arr = [];
 		let container = document.querySelectorAll('.input-div-caja-eleccion');
@@ -324,6 +328,14 @@
 			}
 		})
 	}
+
+	$('#button-candidatos').click(function() {
+			let nombre = $('select[name=candidatos-eleccion] option:selected').text();
+			let id = $('select[name=candidatos-eleccion]').val();
+			let html = '<input class="input-div-caja-candidato" type="text" name="candidato-' + id + '" placeholder="' + nombre + '" readonly><a name="borrar-' + id + '" onclick="borrarInputEleccion(\'' + id + '\',\'candidato\')"><i class="fas fa-window-close"></i></a>';
+			if (!$("#candidatos-div-eleccion input[name=candidato-" + id + "]").length)
+				$('#candidatos-div-eleccion').append(html);
+	});
 
 	function guardarCandidatos() {
 		let arr = [];
